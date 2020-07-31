0d131af20dc1cf25b16ddc55a3eaaddf569fec81
justaino <55673339+justaino@users.noreply.github.com>
2020-07-31 19:50:07 +0100

    AppData Files

---
+++ App Data/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce_Website
+{
+    public class Product
+    {
+
+        public int ItemID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public double price { get; set; }
+
+        public byte[] Image { get; set; }
+
+
+        public Product()
+        {
+
+        }
+
+    }
+}
---
+++ App Data/Trolley.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce_Website
+{
+    public class Trolley
+    {
+        public Trolley()
+        {
+
+        }
+
+        List<Product> products;
+
+        public List<Product> GetTrolleyContent()
+        {
+            products = (List<Product>)HttpContext.Current.Session["Trolley"];
+
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+            return products;
+
+        }
+
+        public void SaveTrolleyContent()
+        {
+            HttpContext.Current.Session["Trolley"] = products;
+        }
+
+        public void AddProduct(Product product)
+        {
+            products = GetTrolleyContent();
+
+            products.Add(product);
+
+            SaveTrolleyContent();
+        }
+
+        public int GetProductCount()
+        {
+            int noOfProducts = 0;
+
+            products = GetTrolleyContent();
+
+            noOfProducts = products.Count();
+
+            return noOfProducts;
+        }
+
+        public double GetPriceTotal()
+        {
+            double total = 0;
+
+            products = GetTrolleyContent();
+
+            foreach (var content in products)
+            {
+                total = total + content.price;
+            }
+
+            return total;
+        }
+
+        public void EmptyTrolley()
+        {
+            products = (List<Product>)HttpContext.Current.Session["Trolley"];
+
+            if (products != null)
+            {
+                products = new List<Product>();
+            }
+            SaveTrolleyContent();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            products = GetTrolleyContent();
+
+            var productToRemove = products.FindIndex(content => content.ItemID == product.ItemID);
+
+            products.RemoveAt(productToRemove);
+
+            SaveTrolleyContent();
+        }
+
+    }
+}
